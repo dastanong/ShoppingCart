023f70d0af384d345504414b0c74b5118d78abb4
Ong <Ong_Chun_Tat_Dastan@dell.com>
2019-04-09 01:52:11 +0800

    solving display cart issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { all } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -16,95 +17,105 @@ export class AppComponent {
         price: 800,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 1,
         price: 1000,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 2,
         price: 600,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 3,
         price: 2000,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 4,
         price: 3000,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 5,
         price: 4000,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 6,
         price: 8000,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 7,
         price: 200,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 8,
         price: 800,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 9,
         price: 500,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 10,
         price: 20,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     },
     {
         id: 11,
         price: 9999,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo autem, modi repudiandae nam alias, minima tempore eum dolores itaque illum vero ipsum nesciunt sed quaerat magni laboriosam magnam? Et, dolorem.",
         image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
-        selected: false
+        selected: false,
+        currentQty: 0
     }
 ]
-  cartList = []
+  allCarts = []
 
   productList = Object.keys(this.allProducts)
-  cart = Object.keys(this.cartList)
+  cartList = Object.keys(this.allCarts)
 
   quantity = 0
-  currentQty = 0
   maxQty = 10
-  selectedProduct : 0
   showProduct : boolean = false
   showCart : boolean = false
   showQty : boolean = false
@@ -115,7 +126,7 @@ export class AppComponent {
         console.log(this.showProduct)
         if(this.showProduct = true) {
             this.showCart = false
-            console.log(this.showCart)
+            //console.log(this.showCart)
         }
     }
 
@@ -124,28 +135,30 @@ export class AppComponent {
         console.log(this.showCart)
         if(this.showCart = true) {
             this.showProduct = false
-            console.log(this.showProduct)
+            //console.log(this.showProduct)
         }
     }
 
-    addQuantity() {
-        if(this.currentQty == 0 || this.currentQty < this.maxQty) {
-            this.quantity++
-            this.currentQty = this.quantity
-            return this.currentQty
-            //console.log(this.currentQty)
+    addQuantity(index: number) {
+        if(index == this.allProducts[index].id && this.allProducts[index].selected == true) {
+            if(this.allProducts[index].currentQty == 0 || this.allProducts[index].currentQty < this.maxQty) {
+                this.quantity++
+                this.allProducts[index].currentQty = this.quantity
+                console.log(this.allProducts[index].currentQty)
+            }
         }
     }
 
-    minusQuantity() {
-        if(this.currentQty > 0) {
-            this.quantity--
-            this.currentQty = this.quantity
-            return this.currentQty
-            //console.log(this.currentQty)
+    minusQuantity(index: number) {
+        if(index == this.allProducts[index].id && this.allProducts[index].selected == true) {
+            if(this.allProducts[index].currentQty > 0) {
+                this.quantity--
+                this.allProducts[index].currentQty = this.quantity
+                console.log(this.allProducts[index].currentQty)
+            }
         }
     }
-
+ 
     checkprdId(index: number) {
         if(index == this.allProducts[index].id) {
             this.allProducts[index].selected = true
@@ -156,6 +169,7 @@ export class AppComponent {
                 console.log(index)
                 console.log(this.allProducts[index].price)
                 console.log("Selected")
+                console.log(this.allProducts[index].selected)
             }
         }
     }
@@ -172,19 +186,24 @@ export class AppComponent {
             this.showQty = false
             this.hideSelect = true
             this.quantity = 0
+            this.allProducts[index].currentQty = this.quantity
             console.log("Unselected")
         }
     }
 
-    addToCart(index: number) {
-        if(index == this.allProducts[index].id && this.allProducts[index].selected == true && this.quantity > 0) {
-            this.cartList = [
-                this.allProducts[index].id,
-                this.allProducts[index].image,
-                this.allProducts[index].description,
-                this.allProducts[index].price
+    addToCart(index: number){
+        if(this.allCarts == [] && this.allProducts[index].selected == true) {
+            this.allCarts = [
+                {
+                    id: this.allProducts[index].id,
+                    image: this.allProducts[index].image,
+                    description: this.allProducts[index].description,
+                    price: this.allProducts[index].price,
+                    currentQty: this.allProducts[index].currentQty
+                },
             ]
-            console.log(this.cartList)
+            console.log(this.allCarts)
+            alert("Item added to Cart. Please Click on My Cart to view your item.")
         }
     }
 
